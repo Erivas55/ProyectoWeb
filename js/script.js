a044c75ceff9a50a040909fc8f951b549580d1c5
Erivas55 <rivaselian17@gmail.com>
2026-02-21 18:45:40 -0600

    last

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
 /* SELECCION DE ELEMENTOS */
- const menuToggle = document.querySelector('.menu-toggle');
- const navLinks = document.querySelector('nav-links');
- const buttons = document.querySelector('.btn');
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+const buttons = document.querySelectorAll('.btn');
 
- /* SELECCION DE ELEMENTOS */
-
-
- /* EVENTO CLICK */
- if(menuToggle && navLinks){
+/* EVENTO CLICK MENU */
+if(menuToggle && navLinks){
     menuToggle.addEventListener('click', function(){
         navLinks.classList.toggle('active');
-    });
- }
-
- /* MANIPULACION DE CLASES */
- if(navLinks){
-    navLinks.classList.add('hidden');
-    navLinks.classList.remove('hidden');
- }
-
- /* EVENTOS A BOTONES */
- buttons.forEach(button =>{
-    button.addEventListener('link', function(){
         console.log('Has hecho clic en el menu');
-        navLinks.classList.toggle('active');
-    })
- })
+    });
+}
